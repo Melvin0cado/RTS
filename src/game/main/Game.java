c1d91f8be188ff97308d3c81c8507d0d0ff426a0
melvin <melvi@192.168.1.74>
2018-06-08 03:11:44 +0200

    organise import

--- src/game/main/Game.java
+++ src/game/main/Game.java
@@ -5,7 +5,6 @@ import javafx.application.Application;
 import javafx.scene.ImageCursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
